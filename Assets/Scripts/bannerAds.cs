ec05a040c16a4d84163995e53ee01e040969ea8d
tarikceylan <tarikceylan@outlook.com.tr>
2020-07-11 20:32:40 +0300

    Integrated Ads

---
+++ Assets/Scripts/bannerAds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class bannerAds : MonoBehaviour
+{
+    private BannerView bannerAdObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize(adStatus => { });
+        bannerAdObj = new BannerView("ca-app-pub-3940256099942544/6300978111", AdSize.SmartBanner, AdPosition.Top); //TEST BANNER AD KEY
+        //bannerAdObj = new BannerView("ca-app-pub-7916658357044225/9321748919", AdSize.SmartBanner, AdPosition.Top); //BANNER AD KEY
+        AdRequest bannerAdRequest = new AdRequest.Builder().Build();
+        bannerAdObj.LoadAd(bannerAdRequest);
+    }
+
+    // Update is called once per frame
+    void OnDestroy()
+    {
+        if (bannerAdObj != null)
+            bannerAdObj.Destroy();
+
+    }
+}
---
+++ Assets/Scripts/interstitialAd.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class interstitialAd : MonoBehaviour
+{
+    public GameObject winnerText;
+    private InterstitialAd interstitialAdObj;
+  
+    void Start()
+    {
+        MobileAds.Initialize( AdStatus => { } );
+        GetNewAd(null, null);
+    }
+    
+    public void GameOver()
+    {
+        if (winnerText.GetComponent<TMPro.TextMeshProUGUI>().text == "YOU WON!" || winnerText.GetComponent<TMPro.TextMeshProUGUI>().text == "COM WON!")
+            StartCoroutine(ShowAd());
+    }
+    
+    IEnumerator ShowAd()
+    {
+        while( !interstitialAdObj.IsLoaded() )
+            yield return null;
+  
+        interstitialAdObj.Show();
+    }
+
+    public void GetNewAd (object sender, EventArgs args)
+    {
+        if (interstitialAdObj != null)
+            interstitialAdObj.Destroy();
+        interstitialAdObj = new InterstitialAd("ca-app-pub-3940256099942544/1033173712"); //INTERSTITIAL TEST AD KEY
+        //interstitialAdObj = new InterstitialAd("ca-app-pub-7916658357044225/2564768874"); //INTERSTITIAL AD KEY
+        interstitialAdObj.OnAdClosed += GetNewAd;
+        AdRequest interAdRequest = new AdRequest.Builder().Build();
+        interstitialAdObj.LoadAd(interAdRequest);
+    }
+  
+    void OnDestroy()
+    {
+        if( interstitialAdObj != null )
+            interstitialAdObj.Destroy();
+    }
+}
